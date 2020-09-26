dd2203402a084e2e4761ae1ffd9220c4c9c7d756
nicorapelas <nicorapelas@gmail.com>
2020-09-26 06:51:50 +0200

    update

--- server/config/keys_prod.js
+++ server/config/keys_prod.js
@@ -1,38 +1,17 @@
 const keys = {
   mongo: {
-    url: function() {
-      return process.env.MONGO_URI
-    },
-    options: {
-      useNewUrlParser: true
-    }
-  },
-  cookie: {
-    secret: process.env.COOKIE_KEY
-  },
-  jwt: {
-    secret: process.env.SECRET_OR_KEY
-  },
-  google: {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET_OR_KEY,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  },
-  facebook: {
-    clientID: process.env.FACEBOOK_CLIENT_ID,
-    clientSecret: process.env.FACEBOOK_SECRET_OR_KEY,
-    callbackURL: process.env.FACEBOOK_CALLBACK
-  },
-  stripe: {
-    clientID: process.env.STRIPE_CLIENT_ID,
-    clientSecret: process.env.STRIPE_SECRET_OR_KEY
+    uri: process.env.MONGO_URI,
   },
   sendgrid: {
-    apiKey: process.env.SEND_GRID_KEY
+    apiKey: process.env.SENDGRID_API_KEY,
+    redirectDomain: process.env.SENDGRID_REDIRECT_DOMAIN,
+    user: process.env.SENDGRID_USER,
+    pass: process.env.SENDGRID_USER,
   },
-  domain: {
-    url: process.env.DOMAIN_URL
-  }
+  payFast: {
+    MerchantID: process.env.PAYFAST_MERCHART_ID,
+    MerchantKey: process.env.PAYFAST_MERCHANT_KEY,
+  },
 }
 
 exports.keys = keys
