74976075bb97fa4ff0de8f20e037b70a1e0e46d5
nicorapelas <nicorapelas@gmail.com>
2020-09-26 07:20:22 +0200

    update

--- server/config/keys_prod.js
+++ server/config/keys_prod.js
@@ -6,7 +6,7 @@ const keys = {
     apiKey: process.env.SENDGRID_API_KEY,
     redirectDomain: process.env.SENDGRID_REDIRECT_DOMAIN,
     user: process.env.SENDGRID_USER,
-    pass: process.env.SENDGRID_USER,
+    pass: process.env.SENDGRID_PASS,
   },
   payFast: {
     MerchantID: process.env.PAYFAST_MERCHART_ID,
